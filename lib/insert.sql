dc8e7b3bd2eb4cd1c40d9c087553fc7b3bcf2050
owen <owen.chosen@gmail.com>
2023-10-17 10:12:07 +0100

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 3, "M", "WHITE", "HIGH", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 3, "M", "WHITE", "HIGH", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 3, "F", "WHITE", "HIGH", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 3, "M", "BLACK", "LOW", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 3, "F", "WHITE", "HIGH", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 3, "M", "WHITE", "HIGH", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 3, "F", "WHITE", "HIGH", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 5, 'M', 'GREY', 'LOW', 0);
